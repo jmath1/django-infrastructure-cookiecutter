1269c1cf3c520ab08692d4987190cc13d564c6df
Jonathan Math <jonathanamath@gmail.com>
2021-10-16 11:16:52 -0500

    fix terraform config

--- {{cookiecutter.workspace}}/terraform/digital_ocean.tf
+++ {{cookiecutter.workspace}}/terraform/digital_ocean.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "{{ project_name }}.terraform"
-    key    = "{{ project_name }}.terraform.tfstate"
+    bucket = "{{ cookiecutter.project_name }}.terraform"
+    key    = "{{ cookiecutter.project_name }}.terraform.tfstate"
     region = "us-east-1"
     shared_credentials_file = "./keys/aws.s3.token"
   }
@@ -27,15 +27,15 @@ resource "digitalocean_ssh_key" "default" {
 # #   Resources   #
 # #################
 
-resource "digitalocean_droplet" "{{ droplet_name }}" {
+resource "digitalocean_droplet" "{{ cookiecutter.droplet_name }}" {
   count    = 3
   image    = var.droplet_os
   name     = "db-consul${count.index+1}"
   region   = var.droplet_region
   size     = var.droplet_size
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
-  {% if add_volume %}
-  volume_ids = [digitalocean_volume.{{ volume_name }}.id]
+  {% if cookiecutter.add_volume %}
+  volume_ids = [digitalocean_volume.{{ cookiecutter.volume_name }}.id]
   {% endif %}
   provisioner "local-exec" {
     command = "./ansible/utils/generate_inventory.py; sleep 120"
@@ -48,11 +48,11 @@ resource "digitalocean_droplet" "{{ droplet_name }}" {
   }
 }
 
-{% if add_volume %}
-resource "digitalocean_volume" "{{ volume_name }}" {
+{% if cookiecutter.add_volume %}
+resource "digitalocean_volume" "{{ cookiecutter.volume_name }}" {
   count                   = 0
   region                  = "nyc1"
-  name                    = "{{ volume_name }}"
+  name                    = "{{ cookiecutter.volume_name }}"
   size                    = 100
   initial_filesystem_type = "ext4"
   description             = "My volume"
